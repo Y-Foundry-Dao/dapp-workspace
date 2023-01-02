fb945f846e3649074234f872b6328e77a6473fdb
Trenten <trenten.grede@gmail.com>
2023-01-02 17:19:29 -0600

    Added variables for contract addresses and chain information

--- src/utilities/variables.ts
+++ src/utilities/variables.ts
@@ -5,3 +5,11 @@ export const PATH_PROFILE_PFP_DEFAULT = 'https://y-foundry-dao.github.io/yfd-dap
 export const PATH_PROFILE_SUFFIX = '.json';
 export const PATH_PROFILE_PFP_SUFFIX = '.png';
 export const PATH_TEST = 'https://y-foundry-dao.github.io/yfd-dapp-profiles/profile/terra1upleyfx24jehpgfy9d79d9scps20ffuf6vy706.json';
+
+export const URL_LCD = 'https://pisco-lcd.terra.dev';
+export const CHAIN_ID = 'pisco-1';
+
+export const YFD_TEST =
+  'terra1293l9rgqk5vxndaeqmengssg3pdch57qpepgslh7p2sg3lqd87rss8du4s';
+export const FORGE_TEST =
+  'terra1s5hg4kusnnp5q8r8l0das4tftd50xcve4e2l95eqjy3fgducekfsw6yder';  
