ef4eb840045b8a7d074502af9cf628fc1703f0bb
Trenten <trenten.grede@gmail.com>
2023-01-02 17:23:10 -0600

    address call in app file so the connected wallet address saves onLoad

--- src/layouts/App.tsx
+++ src/layouts/App.tsx
@@ -2,8 +2,10 @@ import AppContent from '@pages/App';
 import styles from "@scss/app.module.scss";
 import Navigation from '@features/Navigation';
 import MainRoutes from '@routes/Main';
+import useWallet from '@hooks/useWallet';
 
 export default function AppLayout() {
+  const address = useWallet();
   return (
     <div className={styles.App}>
       <header className={styles['App-header']}>
